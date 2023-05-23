820b9a86a6fa6694bf7671d045bbe08a8fef809f
IWasYourTomb <gm@glavstudio.pro>
2023-05-23 11:34:45 +0300

    Initial Commit

---
+++ api/apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiApp.swift
+//  api
+//
+//  Created by 1 on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
